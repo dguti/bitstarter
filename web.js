1373ab5a063c827e02e9f19ffc54356bab2a02d6
dguti <d.gutierrez.perez@gmail.com>
2013-07-16 20:26:12 +0200

    HW4 Step 0-1 NAVBAR

--- web.js
+++ web.js
@@ -8,7 +8,9 @@ app.get('/', function(request, response) {
   response.send(buffer.toString());
 });
 
-var port = process.env.PORT || 5000;
+app.use(express.static(__dirname + '/public'));
+
+var port = process.env.PORT || 8080;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
